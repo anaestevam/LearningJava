60948ac2750bd4bc6553dbc3a279e9df8b6fd3a8
Ana Luisa <53523588+anaestevam@users.noreply.github.com>
2022-08-03 16:49:02 -0300

    a

--- curso/src/Exestrutural.java
+++ curso/src/Exestrutural.java
@@ -5,14 +5,13 @@ public class Exestrutural {
 	public static void main(String[] args){
 		
 		Scanner sc = new Scanner(System.in);
-		/*-------1---------
+		/*-------URI 1003---------
 		 * Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
 		   mensagem explicativa.
 		 */
 		int a;
 		int b;
 		
-		System.out.print("Digite valor de A:");
 		System.out.print("Digite valor de A:");
 		a = sc.nextInt(); //receber o valor a partir do teclado nextLine ler a linha inteira
 		System.out.print("Digite valor de B:");
@@ -31,11 +30,32 @@ public class Exestrutural {
 		double raio;
 		double pi = 3.14159;
 		
-		System.out.println("Digite valor de A:");
+		System.out.println("Digite valor de C:");
 		c = sc.nextDouble(); //receber o valor a partir do teclado nextLine ler a linha inteira
 		raio = pi * Math.pow(c, 2.0);
 		
 		System.out.println("Resultado da area: " + raio);
+		
+		/*-------3---------
+		 * Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+			de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D)
+		 */
+		/*
+		int d,e,f,g, diferenca;
+		System.out.print("Digite valor de D:");
+		d = sc.nextInt();
+		System.out.print("Digite valor de E:");
+		e = sc.nextInt();
+		System.out.print("Digite valor de F:");
+		f = sc.nextInt();
+		System.out.print("Digite valor de G:");
+		g = sc.nextInt();
+		
+		diferenca = (d*e - f*g);
+		
+		System.out.print("DIFERENCA = " + diferenca);  */
+		
+		
 		
 		sc.close();
 	}
