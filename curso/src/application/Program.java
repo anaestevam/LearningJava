a10609d111aaed9e9c84a84d12e1672f07d08fa9
Ana Luisa <53523588+anaestevam@users.noreply.github.com>
2022-08-19 17:00:58 -0300

    Create Program.java

---
+++ curso/src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double x1, x2, x3, y1, y2, y3;
+		
+		System.out.println("Digite o valor das medidas do triangulo X:");
+		x1 = sc.nextDouble();
+		x2 = sc.nextDouble();
+		x3 = sc.nextDouble();
+		System.out.println("Digite o valor das medidas do triangulo Y:");
+		y1 = sc.nextDouble();
+		y2 = sc.nextDouble();
+		y3 = sc.nextDouble();
+
+	}
+
+}
